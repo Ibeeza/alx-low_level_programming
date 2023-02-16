7c525cf767659dbe4c8094510b3f74c1ac4009a9
Ibeeza <ndambirirchard1@gmail.com>
2023-02-16 14:22:57 -0800

    huh

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- a program printing a line using fuction puts
+ *
+ * return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
