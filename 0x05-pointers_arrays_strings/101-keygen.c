b2fa223c4675df8b4dd3136dcf502a5ae9ae06ff
ibeeza <ephesiansokoth@gmail.com>
2023-02-27 19:32:33 +0300

    11-check

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,42 +9,25 @@
 
 int main(void)
 {
-char pass[84];
-int index = 0;
-int sum = 0;
-int half1, half2;
+char pass[100];
+int index, sum, num;
 
-srand(time(0));
+sum = 0;
 
-while (sum < 2772)
+srand(time(NULL));
+
+for (index = 0 ; index < 100 ; index++)
 {
-pass[index] = 33 + rand() % 94;
-sum += pass[index++];
-}
-pass[index] = '\0';
-if (sum != 2772)
+pass[index] = rand() % 78;
+sum += (pass[index] + '0');
+putchar(pass[index] + '0');
+for ((2772 - sum) - '0' < 78)
 {
-half1 = (sum - 2772) / 2;
-half2 = (sum - 2772) / 2;
-if ((sum -2772) % 2 != 0)
-half1++;
-for (index = 0 ; pass[index]; index++)
-{
-if (pass[index] >= (33 + half1))
-{
-pass[index] -= half1;
+num = 2772 - sum - '0';
+sum += num;
+putchar(num + '0');
 break;
 }
 }
-for (index = 0 ; pass[index]; index++)
-{
-if (pass[index] >= (33 + half2))
-{
-pass[index] -= half2;
-break;
-}
-}
-}
-printf("%s", pass);
 return (0);
 }
