166495767e59fa894d4435a01a10816b698adf33
ibeeza <ephesiansokoth@gmail.com>
2023-02-27 17:46:52 +0300

    2 check betty

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
+ * _strlen - that function
  * _strlen: check my string length won't you
  * @s:paramenter in use
- * Return- the lenght of string
+ * Return: the lenght of string
 */
 
 int _strlen(char *s)
 {
 int w = 0;
 
-while (*s != '\0')
+while (*s++)
 {
 w++;
-s++;
 }
 return (w);
 }
+
