5a36764e6687bccafd3d9aa74796ad14841611a8
ibeeza <ndambiririchard1@gmail.com>
2023-03-02 17:26:50 +0300

    check 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,36 +4,36 @@
 /**
  * cap_string- cap cap my words
  * cap_string: capitalize these words
- * @st: parameter used
+ * @str: parameter used
  * Return: my value
 */
 
-char *cap_string(char *st)
+char *cap_string(char *str)
 {
 	int num = 0;
 
-	while (st[num])
+	while (str[num])
 	{
-		while (!(st[num] >= 'a' && st[num] <= 'z'))
+		while (!(str[num] >= 'a' && str[num] <= 'z'))
 			num++;
-		if (st[num - 1] == ' ' ||
-		st[num - 1] == '\t' ||
-		st[num - 1] == '\n' ||
-		st[num - 1] == ',' ||
-		st[num - 1] == ';' ||
-		st[num - 1] == '.' ||
-		st[num - 1] == '!' ||
-		st[num - 1] == '?' ||
-		st[num - 1] == '"' ||
-		st[num - 1] == '(' ||
-		st[num - 1] == ')' ||
-		st[num - 1] == '{' ||
-		st[num - 1] == '}' ||
-		st[num - 1] == 0)
-			st[num] -= 32;
+		if (str[num - 1] == ' ' ||
+		str[num - 1] == '\t' ||
+		str[num - 1] == '\n' ||
+		str[num - 1] == ',' ||
+		str[num - 1] == ';' ||
+		str[num - 1] == '.' ||
+		str[num - 1] == '!' ||
+		str[num - 1] == '?' ||
+		str[num - 1] == '"' ||
+		str[num - 1] == '(' ||
+		str[num - 1] == ')' ||
+		str[num - 1] == '{' ||
+		str[num - 1] == '}' ||
+		str[num - 1] == 0)
+			str[num] -= 32;
 		num++;
 	}
-	return (st);
+	return (str);
 }
 
 
