a2d447ea171f793186fd82a56018c20f09159de7
ibeeza <ndambiririchard1@gmail.com>
2023-03-02 20:00:10 +0300

    last last

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,40 +1,57 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_buffer - prints buffer
- * @b: buffer
- * @size: par
+ * print_line - prints a s bytes of a buffer
+ * @c: buffer to print
+ * @s: bytes of buffer to print
+ * @l: line of buffer to print
+ *
  * Return: void
-*/
+ */
+void print_line(char *c, int s, int l)
+{
+int j, k;
+for (j = 0; j <= 9; j++)
+{
+if (j <= s)
+printf("%02x", c[l * 10 + j]);
+else
+printf("  ");
+if (j % 2)
+putchar(' ');
+}
+for (k = 0; k <= s; k++)
+{
+if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+putchar(c[l * 10 + k]);
+else
+putchar('.');
+}
+}
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer to print
+ * @size: size of buffer
+ *
+ * Return: void
+ */
 void print_buffer(char *b, int size)
 {
-	int byte, in;
-
-	for (byte = 0 ; byte < size ; byte += 10)
-	{
-		printf("%08x: ", byte);
-		for (in = 0 ; in < 10 ; in++)
-		{
-		if ((in + byte) >= size)
-			printf(" ");
-		else
-			printf("%02x", *(b + in + byte));
-		if ((in % 2) != 0 && in != 0)
-			printf(" ");
-		}
-		for (in = 0 ; in < 10 ; in++)
-		{
-			if ((in + byte) >= size)
-				break;
-			else if (*(b + in + byte) >= 31 && *(b + in + byte) <= 126)
-				printf("%c", *(b + in + byte));
-			else
-				printf(".");
-		}
-		if (byte >= size)
-			continue;
-		printf("\n");
-	}
-	if (size <= 0)
-		printf("\n");
+int i;
+for (i = 0; i <= (size - 1) / 10 && size; i++)
+{
+printf("%08x: ", i * 10);
+if (i < size / 10)
+{
+print_line(b, 9, i);
 }
+else
+{
+print_line(b, size % 10 - 1, i);
+}
+putchar('\n');
+}
+if (size == 0)
+putchar('\n');
+}
+
