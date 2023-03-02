ac7eeb2ef3f075ddb4e030d9fa43dd5f50035b80
ibeeza <ndambiririchard1@gmail.com>
2023-03-02 15:54:44 +0300

    6-check

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,17 +10,30 @@
 
 char *cap_string(char *st)
 {
-int num = 0;
+	int num = 0;
 
-while (st[num])
-{
-while (!(st[num] >= 'a' && st[num] <= 'z'))
-num++;
-if (st[num - 1] == ' ' || st[num - 1] == '\t' || st[num - 1] == '\n' || st[num - 1] == ',' || st[num - 1] == ';' || st[num - 1] == '.' || st[num - 1] == '!' || st[num - 1] == '?' || st[num - 1] == '"' || st[num - 1] == '(' || st[num - 1] == ')' || st[num - 1] == '{' || st[num - 1] == '}' || st[num - 1] == 0) 
-{
-st[num] -= 32;
-num++;
+	while (st[num])
+	{
+	while (!(st[num] >= 'a' && st[num] <= 'z'))
+	num++;
+	if (st[num - 1] == ' ' ||
+	st[num - 1] == '\t' ||
+	st[num - 1] == '\n' ||
+	st[num - 1] == ',' ||
+	st[num - 1] == ';' ||
+	st[num - 1] == '.' ||
+	st[num - 1] == '!' ||
+	st[num - 1] == '?' ||
+	st[num - 1] == '"' ||
+	st[num - 1] == '(' ||
+	st[num - 1] == ')' ||
+	st[num - 1] == '{' ||
+	st[num - 1] == '}' ||
+	st[num - 1] == 0)
+	st[num] -= 32;
+	num++;
+	}
+	return (st);
 }
-}
-return (st);
-}
+
+
